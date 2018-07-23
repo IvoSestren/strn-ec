01633d5afadf94c3a0f268992a6c810e90fd4c23
Ivo Sestren Junior <ivo@sestren.com.br>
2018-07-23 16:31:33 -0300

    Funcionando

---
+++ core/api/src/main/java/br/com/strn/ec/core/api/controller/TokenController.java
@@ -0,0 +1,31 @@
+package br.com.strn.ec.core.api.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.view.Results;
+
+import javax.inject.Inject;
+
+@Controller
+@Path("/token")
+public class TokenController {
+
+    private final Result result;
+
+    protected TokenController() {
+        this(null);
+    }
+
+    @Inject
+    public TokenController(Result result) {
+        this.result = result;
+    }
+
+    @Get
+    @Path("/")
+    public void index() {
+        result.use(Results.http()).body("Oi");
+    }
+}
